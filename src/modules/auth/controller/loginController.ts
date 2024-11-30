aa12d0688166cf44444df66593211e67a2bb8365
Henry Goes Zanin <henrygoeszaninpro@gmail.com>
2024-11-30 15:16:21 -0300

    feat - erros personalizdos no login

--- src/modules/auth/controller/loginController.ts
+++ src/modules/auth/controller/loginController.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 import { loginHandler } from '../handler/loginHandler';
+import { UnauthorizedError } from '../../../error/errors';
 
 // Declaração do método de autenticação no Fastify
 declare module 'fastify' {
@@ -12,6 +13,14 @@ declare module 'fastify' {
 export const loginController = (fastify: FastifyInstance) => {
   fastify.post('/login', async (request: FastifyRequest, reply: FastifyReply) => {
     fastify.log.info('User login attempt');
-    return loginHandler(request, reply);
+    try {
+      return await loginHandler(request, reply);
+    } catch (error) {
+      fastify.log.error(error);
+      if (error instanceof UnauthorizedError) {
+        return reply.status(401).send({ error: error.message });
+      }
+      return reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
 };
--- src/modules/auth/handler/loginHandler.ts
+++ src/modules/auth/handler/loginHandler.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import bcrypt from 'bcrypt';
+import { UnauthorizedError } from '../../../error/errors';
 
 const prisma = new PrismaClient();
 
@@ -11,11 +12,9 @@ export const loginHandler = async (request: FastifyRequest, reply: FastifyReply)
     where: { email },
   });
 
-  console.log(user);
-
   // Verifica se o usuário existe e se a senha está correta
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return reply.status(401).send({ error: 'Invalid email or password' });
+    throw new UnauthorizedError('Invalid email or password');
   }
 
   // Gera um token JWT para o usuário
