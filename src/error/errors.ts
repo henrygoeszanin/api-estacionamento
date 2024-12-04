256a88a1db58539b7ab8953810180bc4f32c8414
Henry Goes Zanin <henrygoeszaninpro@gmail.com>
2024-12-04 00:26:19 -0300

    refact - errors

--- src/error/errors.ts
+++ src/error/errors.ts
@@ -8,9 +8,10 @@ export class AppError extends Error {
   }
 }
 
-export class NotFoundError extends AppError {
-  constructor(message = 'Resource not found') {
-    super(message, 404);
+// Erros da família 400
+export class BadRequestError extends AppError {
+  constructor(message = 'Bad Request') {
+    super(message, 400);
   }
 }
 
@@ -26,14 +27,63 @@ export class ForbiddenError extends AppError {
   }
 }
 
-export class BadRequestError extends AppError {
-  constructor(message = 'Bad Request') {
-    super(message, 400);
+export class NotFoundError extends AppError {
+  constructor(message = 'Resource not found') {
+    super(message, 404);
   }
 }
 
+export class MethodNotAllowedError extends AppError {
+  constructor(message = 'Method Not Allowed') {
+    super(message, 405);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message = 'Conflict') {
+    super(message, 409);
+  }
+}
+
+export class UnprocessableEntityError extends AppError {
+  constructor(message = 'Unprocessable Entity') {
+    super(message, 422);
+  }
+}
+
+export class TooManyRequestsError extends AppError {
+  constructor(message = 'Too Many Requests') {
+    super(message, 429);
+  }
+}
+
+// Erros da família 500
 export class InternalServerError extends AppError {
   constructor(message = 'Internal Server Error') {
     super(message, 500);
+  }
+}
+
+export class NotImplementedError extends AppError {
+  constructor(message = 'Not Implemented') {
+    super(message, 501);
+  }
+}
+
+export class BadGatewayError extends AppError {
+  constructor(message = 'Bad Gateway') {
+    super(message, 502);
+  }
+}
+
+export class ServiceUnavailableError extends AppError {
+  constructor(message = 'Service Unavailable') {
+    super(message, 503);
+  }
+}
+
+export class GatewayTimeoutError extends AppError {
+  constructor(message = 'Gateway Timeout') {
+    super(message, 504);
   }
 }
